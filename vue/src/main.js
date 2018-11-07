255b89cd03e1beb2b0fb8c21cd6ca0a45f599819
ryoju-ohata <ryoju@example.com>
2018-11-07 21:13:32 +0900

    Fix md preview

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../node_modules/github-markdown-css/github-markdown.css'
 
 Vue.config.productionTip = false
 
--- vue/src/views/Sp.vue
+++ vue/src/views/Sp.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-html="html"></div>
+    <div class="markdown-body" v-html="html"></div>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ import Marked from 'marked'
 export default {
   name: 'edit',
   created () {
-    fetch('http://dms-vue-test.basement.jp/sp.php').then(response => response.text()).then((res) => {
+    fetch('https://raw.githubusercontent.com/passionate-engineer/lumen-vue-admin-practice/master/SUBJECT_JP.md').then(response => response.text()).then((res) => {
       this.html = this.marked(res)
     })
   },
@@ -29,4 +29,17 @@ export default {
 </script>
 
 <style scoped>
+.markdown-body {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+}
+
+@media (max-width: 767px) {
+  .markdown-body {
+    padding: 15px;
+  }
+}
 </style>
